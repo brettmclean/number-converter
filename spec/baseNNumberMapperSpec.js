488f3be61c3431c7d3e26b67f1e90c9661aa6ea4
Brett McLean <brettrmclean@gmail.com>
2015-08-16 20:15:54 -0600

    Ensure Base-N numbers are not negative

--- spec/baseNNumberMapperSpec.js
+++ spec/baseNNumberMapperSpec.js
@@ -9,4 +9,20 @@ describe("A Base-N Number Mapper", function() {
 		expect(typeof bnnm.fromDecimal).toBe("function");
 	});
 
+	it("should throw a RangeError when given a negative number", function() {
+		var bnnm = new BaseNNumberMapper();
+
+		expect(function() {
+			bnnm.fromDecimal(-5);
+		}).toThrowError(RangeError);
+
+		expect(function() {
+			bnnm.toDecimal(-5);
+		}).toThrowError(RangeError);
+
+		expect(function() {
+			bnnm.fromDecimal("-10011010");
+		}).toThrowError(RangeError);
+	});
+
 });
