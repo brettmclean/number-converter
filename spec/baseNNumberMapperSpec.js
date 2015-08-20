8ca912bef3a7b070365e1e42c97f21037fd0959e
Brett McLean <brettrmclean@gmail.com>
2015-08-19 21:18:17 -0600

    Add tests for BaseNNumberMapper with various bases

--- spec/baseNNumberMapperSpec.js
+++ spec/baseNNumberMapperSpec.js
@@ -49,6 +49,38 @@ describe("A Base-N Number Mapper", function() {
 		expect(baseNNumberMapper.toDecimal(11111111)).toBe(255);
 	});
 
+	it("can convert octal numbers to decimal", function() {
+		var baseNNumberMapper = new BaseNNumberMapper(8);
+
+		expect(baseNNumberMapper.toDecimal(0)).toBe(0);
+		expect(baseNNumberMapper.toDecimal(7)).toBe(7);
+		expect(baseNNumberMapper.toDecimal(123)).toBe(83);
+		expect(baseNNumberMapper.toDecimal(765)).toBe(501);
+		expect(baseNNumberMapper.toDecimal(1750)).toBe(1000);
+		expect(baseNNumberMapper.toDecimal(23420)).toBe(10000);
+	});
+
+	it("can convert hexadecimal numbers to decimal", function() {
+		var baseNNumberMapper = new BaseNNumberMapper(16);
+
+		expect(baseNNumberMapper.toDecimal(0)).toBe(0);
+		expect(baseNNumberMapper.toDecimal("F")).toBe(15);
+		expect(baseNNumberMapper.toDecimal("BEEF")).toBe(48879);
+		expect(baseNNumberMapper.toDecimal("ABCDEF")).toBe(11259375);
+		expect(baseNNumberMapper.toDecimal("FABF00D")).toBe(262926349);
+		expect(baseNNumberMapper.toDecimal(123456789)).toBe(4886718345);
+	});
+
+	it("can convert base-36 numbers to decimal", function() {
+		var baseNNumberMapper = new BaseNNumberMapper(36);
+
+		expect(baseNNumberMapper.toDecimal(0)).toBe(0);
+		expect(baseNNumberMapper.toDecimal("Z")).toBe(35);
+		expect(baseNNumberMapper.toDecimal("00000Z")).toBe(35);
+		expect(baseNNumberMapper.toDecimal("AZ")).toBe(395);
+		expect(baseNNumberMapper.toDecimal("Z00")).toBe(45360);
+	});
+
 	it("can accept numbers of type string or number", function() {
 		var baseNNumberMapper = new BaseNNumberMapper(2);
 
