8402054dc8d404451e3cea44bed4ac0f320e27fd
Brett McLean <brettrmclean@gmail.com>
2015-08-29 23:33:08 -0600

    Support converting negative numbers to scientific notation

--- spec/scientificNotationMapperSpec.js
+++ spec/scientificNotationMapperSpec.js
@@ -32,12 +32,6 @@ describe("A Scientific Notation Mapper", function() {
 		expect(snm.fromDecimal(984.180001)).toBe("9.84180001e2");
 	});
 
-	it("can convert one to scientific notation", function() {
-		var snm = new ScientificNotationMapper();
-
-		expect(snm.fromDecimal(1)).toBe("1e0");
-	});
-
 	it("can convert positive decimal numbers less than one to scientific notation", function() {
 		var snm = new ScientificNotationMapper();
 
@@ -49,10 +43,50 @@ describe("A Scientific Notation Mapper", function() {
 		expect(snm.fromDecimal(0.0000025)).toBe("2.5e-6");
 	});
 
+	it("can convert whole decimal numbers less than negative one to scientific notation", function() {
+		var snm = new ScientificNotationMapper();
+
+		expect(snm.fromDecimal(-3)).toBe("-3e0");
+		expect(snm.fromDecimal(-500)).toBe("-5e2");
+		expect(snm.fromDecimal(-10000000)).toBe("-1e7");
+
+		expect(snm.fromDecimal(-3141592654)).toBe("-3.141592654e9");
+		expect(snm.fromDecimal(-12345)).toBe("-1.2345e4");
+	});
+
+	it("can convert fractional decimal numbers less than negative one to scientific notation", function() {
+		var snm = new ScientificNotationMapper();
+
+		expect(snm.fromDecimal(-1.2345)).toBe("-1.2345e0");
+		expect(snm.fromDecimal(-12.345)).toBe("-1.2345e1");
+		expect(snm.fromDecimal(-123.45)).toBe("-1.2345e2");
+		expect(snm.fromDecimal(-1234.5)).toBe("-1.2345e3");
+
+		expect(snm.fromDecimal(-984.18000)).toBe("-9.8418e2");
+		expect(snm.fromDecimal(-984.180001)).toBe("-9.84180001e2");
+	});
+
+	it("can convert negative decimal numbers greater than negative one to scientific notation", function() {
+		var snm = new ScientificNotationMapper();
+
+		expect(snm.fromDecimal(-0.99999)).toBe("-9.9999e-1");
+		expect(snm.fromDecimal(-0.5)).toBe("-5e-1");
+		expect(snm.fromDecimal(-0.12345)).toBe("-1.2345e-1");
+		expect(snm.fromDecimal(-0.0000001)).toBe("-1e-7");
+		expect(snm.fromDecimal(-0.00101010)).toBe("-1.0101e-3");
+		expect(snm.fromDecimal(-0.0000025)).toBe("-2.5e-6");
+	});
+
 	it("can convert zero to scientific notation", function() {
 		var snm = new ScientificNotationMapper();
 
 		expect(snm.fromDecimal(0)).toBe("0e0");
+	});
+
+	it("can convert one to scientific notation", function() {
+		var snm = new ScientificNotationMapper();
+
+		expect(snm.fromDecimal(1)).toBe("1e0");
 	});
 
 });
