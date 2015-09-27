101f071ba74acb97fcef9c7b4ad6b633ee85eab6
Brett McLean <brettrmclean@gmail.com>
2015-09-26 23:25:13 -0600

    Add option for whether to append newline character in cliApp

--- spec/cliSpec.js
+++ spec/cliSpec.js
@@ -1,17 +1,15 @@
+var os = require("os");
 var ReadableTestData = require("./utils/ReadableTestData");
 var WritableTestResults = require("./utils/WritableTestResults");
 var cliApp = require("../lib/cli/app");
 
-var runCliAppAndGetOutput = function(args, inputData, outputResults) {
+var runCliAppAndGetOutput = function(args, inputData, outputResults, options) {
 	args = args || "";
 	inputData = inputData || new ReadableTestData();
 	outputResults = new WritableTestResults();
-	cliApp.run(args, inputData, outputResults);
-	return outputResults.getData();
-};
 
-var trimFinalNewline = function(str) {
-	return str.replace(/\n+$/, "");
+	cliApp.run(args, inputData, outputResults, options);
+	return outputResults.getData();
 };
 
 describe("The command-line application", function() {
@@ -23,11 +21,23 @@ describe("The command-line application", function() {
 	it("should output a number when given a number as only argument", function() {
 		var number = "3";
 		var output = runCliAppAndGetOutput([number]);
-		expect(trimFinalNewline(output)).toBe(number);
+		expect(output).toBe(number);
 
 		number = "6745.167";
 		output = runCliAppAndGetOutput([number]);
-		expect(trimFinalNewline(output)).toBe(number);
+		expect(output).toBe(number);
+	});
+
+	it("should output a trailing new line when requested", function() {
+		var number = "1234";
+		var options = {
+			appendNewLine: true
+		};
+		var expectedOutput = number + os.EOL;
+
+		var output = runCliAppAndGetOutput([number], null, null, options);
+
+		expect(output).toBe(expectedOutput);
 	});
 
 });
