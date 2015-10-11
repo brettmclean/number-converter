9726e9743fdb6c11a300e1b4a72874d9b722a646
Brett McLean <brettrmclean@gmail.com>
2015-10-10 19:47:12 -0600

    Support data entering input stream in multiple chunks

--- spec/utils/ReadableTestData.js
+++ spec/utils/ReadableTestData.js
@@ -5,10 +5,13 @@ var ReadableTestData = function(testData, options) {
 	Readable.call(this);
 
 	this._data = null;
+	this._streamIndex = 0;
 	if(typeof testData === "string") {
 		this._data = [testData];
 	} else if(Array.isArray(testData)) {
 		this._data = testData;
+	} else {
+		this._data = [];
 	}
 
 	options = options || {};
@@ -19,11 +22,13 @@ util.inherits(ReadableTestData, Readable);
 ReadableTestData.prototype._read = function() {
 	var lineEnding = this._useWindowsLineEndings ? "\r\n" : "\n";
 
-	if(this._data) {
-		var str = this._data.join(lineEnding) + lineEnding;
-		this.push(str);
+	if(this._streamIndex < this._data.length) {
+		var str = this._data[this._streamIndex];
+		this.push(str + lineEnding);
+		this._streamIndex++;
+	} else {
+		this.push(null);
 	}
-	this.push(null);
 };
 
 module.exports = ReadableTestData;
